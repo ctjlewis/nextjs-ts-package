21f04b898b95d5493825b5cb5c9f998c232d40a4
Lewis <lewis@spellcraft.org>
2024-12-23 11:58:14 -0600

    solution: use `transpilePackages` in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["ts-package-example"],
 };
 
 export default nextConfig;
